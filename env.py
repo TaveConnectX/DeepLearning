6df9834429d4beae781b70a3d6bb4b5f2bd4deb6
sangyeon-park <tkddus0421@gmail.com>
2023-05-15 18:29:56 +0900

    applying CNN, adding some annotations

--- env.py
+++ env.py
@@ -5,31 +5,43 @@ import copy
 from collections import deque
 import torch
 from torch import nn, optim
+import torch.nn.init as init
+import torch.nn.functional as F
 
+# console 창을 비우는 함수 
 def clear():
     os.system('cls' if os.name=='nt' else 'clear')
 
+
+# 가장 기본적인 connect4 게임 환경 
 class ConnectFourEnv:
-    def __init__(self, n_row=6, n_col=7, mode='cpu'):
+    def __init__(self, n_row=6, n_col=7,first_player=None):
         self.n_row = n_row
         self.n_col = n_col
         self.board = np.zeros((n_row, n_col))
-        self.mode = mode
-        self.player = np.random.choice([1,2])
+        if first_player is None:
+            self.player = np.random.choice([1,2])
+        else: self.player = first_player
+        # 만약 경기가 끝나면 win은 player 가 됨, 비길 경우 3 
         self.win = 0
         self.done = False
+        # 가능한 actions들. 꽉찬 column엔 piece를 더 넣을 수 없기 때문 
         self.valid_actions = [i for i in range(self.n_col)]
 
-    def reverse_piece(self, board):
-        board = np.where(
-                board == 1,2, \
-                np.where(board == 2,1, board)
-        )
-        return board
+    # board_normalization() 함수로 대체 예정 
+    # def reverse_piece(self, board):
+    #     board = np.where(
+    #             board == 1,2, \
+    #             np.where(board == 2,1, board)
+    #     )
+    #     return board
 
-    def reset(self):
+    # 게임이 끝났을 때 새로운 환경을 생성하는 대신 reset()으로 처리 
+    def reset(self, first_player=None):
         self.board = np.zeros((self.n_row, self.n_col))
-        self.player = np.random.choice([1,2])
+        if first_player is None: self.player = np.random.choice([1,2])
+        else: self.player = first_player
+
         self.win = 0
         self.done = False
         self.valid_actions = [i for i in range(self.n_col)]
@@ -37,6 +49,8 @@ class ConnectFourEnv:
     # col에 조각 떨어뜨리기 
     def step(self, action):
         col = action
+        # 경기가 끝나지 않을 때 negative reward 를 줄지 말지는 생각이 필요함 
+        # reward = -1/42.
         reward = 0
         # 떨어뜨리려는 곳이 이미 가득 차있을 때
         # 로직을 바꿔서 이젠 이 if문은 실행되지 않을 것임 
@@ -44,23 +58,31 @@ class ConnectFourEnv:
             reward = -1
             print("this cannot be happened")
         else:
+            # piece를 둠 
             for row in range(self.n_row-1,-1,-1):
                 if self.board[row][col] == 0:
                     self.board[row][col] = self.player
                     break
                 else: continue
         
+        # action을 취한 후 해당 column이 꽉차면 valid_action에서 제외 
         if self.board[0,col] != 0:
             if col in self.valid_actions:
                 self.valid_actions.remove(col)
-
+        
+        # action을 취한 후 승패 체크
         self.check_win()
         if self.win != 0:
+            # 비기면 -0.1점 (비겼을 때의 reward도 생각해봐야됨)
             if self.win == 3: reward = -0.1
+            # 이기면 +1점 
             elif self.player == self.win: reward = 1
+            # 진 agent에겐 train 과정에서 따로 negative reward를 부여하기 때문에 해당 elif 문은 필요 없음
             elif self.player != self.win: 
                 reward = -1
                 print("this cannot be happened")
+
+        # 모든 행동이 완료되면 player change
         self.change_player()
 
         return (self.board, reward, self.done)
@@ -80,12 +102,14 @@ class ConnectFourEnv:
                 possible_states.append(cpy_board)
         return possible_states
 
+    # player를 change
     def change_player(self):
         self.player = int(2//self.player)
 
     # chatgpt에게 물어본 보드 출력 함수를 살짝 수정 
-    def print_board(self):
-        clear()
+    # clear_board는 board를 출력하기 전에 console창을 비울지 여부 
+    def print_board(self, clear_board=True):
+        if clear_board: clear()
         board = copy.deepcopy(self.board)
         # if self.player == 2:
         #     board = self.reverse_piece(board)
@@ -166,6 +190,7 @@ class ConnectFourEnv:
     #     if self.win != 0: self.done = True
 
     # made by chatgpt and I edit little bit.
+    # 가로, 세로, 대각선에 완성된 줄이 있는지를 체크한다 
     def check_win(self):
         for i in range(self.n_row):
             for j in range(self.n_col):
@@ -192,6 +217,7 @@ class ConnectFourEnv:
                         return
     
         # no winner
+        # 맨 윗줄이 모두 꽉차있다면, 비긴 것
         if not 0 in self.board[0,:]:
             self.win = 3  # 3 means the game is a draw
             self.done = True
@@ -203,10 +229,12 @@ class ConnectFourEnv:
         self.step(col)
         self.print_board()
 
+    # 그냥 random으로 두고 싶을 때 
     def step_cpu(self):
         self.step(np.random.choice(range(self.n_col)))
         self.print_board()
 
+# this class is for Q-learning, but I don't use it
 class CFAgent:
     def __init__(self, env, lr=0.1, gamma=0.9, epsilon=0.1, policy='q-learning'):
         self.env = env
@@ -255,10 +283,13 @@ class ConnectFourDQNAgent:
             nn.ReLU(),
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
             nn.Linear(hidden_size, action_size)
-        ) # .to(self.device)
+        ).to(self.device)
+
+        # He로 가중치 초기화
+        for layer in self.policy_net:
+            if isinstance(layer, nn.Linear):
+                nn.init.kaiming_normal_(layer.weight, mode='fan_in', nonlinearity='relu')
         
         self.target_net = copy.deepcopy(self.policy_net)
         
@@ -283,10 +314,7 @@ class ConnectFourDQNAgent:
         if np.random.uniform() < self.eps:
             return np.random.choice(valid_actions)
         with torch.no_grad():
-            # 2p이면 보드판을 반전시켜서 보이게 하여, 항상 같은 색깔을 보면서 학습 가능
-            if player == 2: state_ = -1 * state
-            else: state_ = state
-            state_ = torch.FloatTensor(state_)  # .to(self.device)
+            state_ = torch.FloatTensor(state).to(self.device)
             q_value = self.policy_net(state_)
             # print("state:",state)
             # print("valid_actions:",valid_actions)
@@ -303,11 +331,11 @@ class ConnectFourDQNAgent:
         minibatch = random.sample(self.memory, self.batch_size)
 
         
-        state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch])
-        action_batch = torch.Tensor([a for (s1,a,r,s2,d) in minibatch])
-        reward_batch = torch.Tensor([r for (s1,a,r,s2,d) in minibatch])
-        state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch])
-        done_batch = torch.Tensor([d for (s1,a,r,s2,d) in minibatch])
+        state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        action_batch = torch.Tensor([a for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        reward_batch = torch.Tensor([r for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        done_batch = torch.Tensor([d for (s1,a,r,s2,d) in minibatch]).to(self.device)
         Q1 = self.policy_net(state1_batch) 
         with torch.no_grad():
             Q2 = self.target_net(state2_batch) #B
@@ -315,7 +343,7 @@ class ConnectFourDQNAgent:
         Y = reward_batch + self.gamma * ((1-done_batch) * torch.max(Q2,dim=1)[0])
         X = Q1.gather(dim=1,index=action_batch.long().unsqueeze(dim=1)).squeeze()
         loss = nn.MSELoss()(X, Y.detach())
-        self.losses.append(loss.detach().numpy())
+        self.losses.append(loss.detach().cpu().numpy())
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -344,6 +372,199 @@ class ConnectFourDQNAgent:
     def update_target_net(self):
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
+# class 가독성을 높이기 위해 network 부분만 따로 분리 
+class CFCNN(nn.Module):
+    def __init__(self, action_size=7):
+        super(CFCNN,self).__init__()
+        # self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(2,2), stride=1,padding=1)
+        # self.conv2 = nn.Conv2d(32,64,(2,2), stride=1, padding=1)
+        # self.conv3 = nn.Conv2d(64,64,(2,2), stride=1, padding=1)
+        
+        # self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(4,4), stride=1,padding=2)
+        # self.conv2 = nn.Conv2d(32,64,(4,4), stride=1, padding=1)
+        # self.conv3 = nn.Conv2d(64,64,(4,4), stride=1, padding=1)
+        # self.linear1 = nn.Linear(64*3*4, 64)
+        # self.linear2 = nn.Linear(64, action_size)
+
+        self.conv1 = nn.Conv2d(1,42,(4,4), stride=1, padding=2)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.linear1 = nn.Linear(42*3*4, 42)
+        self.linear2 = nn.Linear(42, action_size)
+        
+        self.layers = [
+            self.conv1,
+            # self.conv2,
+            # self.conv3,
+            self.maxpool1,
+            self.linear1,
+            self.linear2
+        ]
+
+        # relu activation 함수를 사용하므로, He 가중치 사용
+        for layer in self.layers:
+            if type(layer) in [nn.Conv2d, nn.Linear]:
+                init.kaiming_normal_(layer.weight, mode='fan_in', nonlinearity='relu')
+            layer.cuda()
+
+
+    def forward(self, x):
+        y = F.relu(self.conv1(x))
+        y = self.maxpool1(y)
+        y = y.flatten(start_dim=2)
+        # view로 채널 차원을 마지막으로 빼줌
+        # 정확한 이유는 나중에 알아봐야 할듯? 
+        y = y.view(y.shape[0], -1, 42)
+        y = y.flatten(start_dim=1)
+        y = F.relu(self.linear1(y))
+        y = self.linear2(y)
+        return y.cuda()
+
+    # def forward(self,x):
+    #     # (N, 1, 6,7)
+    #     y = F.relu(self.conv1(x))
+    #     # (N, 32, 7,8)
+    #     y = F.relu(self.conv2(y))
+    #     # (N, 64, 8,9)
+    #     y = F.relu(self.conv3(y))
+    #     # (N, 64, 9,10)
+    #     #print("shape x after conv:",y.shape)
+    #     y = y.flatten(start_dim=2)
+    #     # (N, 64, 90)
+    #     #print("shape x after flatten:",y.shape)
+    #     y = y.view(y.shape[0], -1, 64)
+    #     # (N, 90, 64)
+    #     #print("shape x after view:",y.shape)
+    #     y = y.flatten(start_dim=1)
+    #     # (N, 90*64)
+    #     y = F.relu(self.linear1(y))
+    #     # (N, 64)
+    #     y = self.linear2(y) # size N, 12
+    #     # (N, 12)
+    #     return y.cuda()
+    
+
+class ConnectFourDQNAgent_CNN(nn.Module):
+    def __init__(self, state_size=6*7, action_size=7, gamma=0.99, lr=0.003, batch_size=1024, target_update=1000, eps=1., memory_len=10000):
+        super(ConnectFourDQNAgent_CNN,self).__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # 실제 업데이트되는 network
+        self.policy_net = CFCNN()
+        # target network
+        self.target_net = copy.deepcopy(self.policy_net)
+        
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+
+
+        # optimizer는 기본적으로 adam을 사용하겠지만 추후 다른 것으로 실험할 수도 있음
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
+        
+        # DQN에 사용될 replay memory(buffer)
+        self.memory = deque(maxlen=memory_len)
+        self.gamma = gamma  # discount factor
+        self.state_size = state_size
+        self.action_size = action_size
+        self.target_update = target_update  # target net update 주기(여기선 step)
+        self.steps = 0
+        self.eps = eps  # DQN에서 사용될 epsilon
+        self.batch_size = batch_size  # size of mini-batch
+        self.losses = []  # loss들을 담는 list 
+
+    # def forward(self, x):
+    #     x = torch.tensor(x).float().to(self.device)
+    #     x = self.conv_net(x)
+    #     x = x.view(x.size(0), -1)
+    #     x = self.fc(x)
+
+    #     return x
+
+        
+    def select_action(self, state, valid_actions=None, player=1):
+        if valid_actions is None:
+            valid_actions = range(self.action_size)
+
+        if np.random.uniform() < self.eps:
+            return np.random.choice(valid_actions)
+        with torch.no_grad():
+            state_ = torch.FloatTensor(state).to(self.device)
+            state_ = state_.unsqueeze(0).unsqueeze(0)  # (6,7) -> (1,1,6,7)
+            q_value = self.policy_net(state_)
+            # print("state:",state)
+            # print("valid_actions:",valid_actions)
+            # print("q_value:",q_value)
+            valid_q_values = q_value.squeeze()[torch.tensor(valid_actions)]
+            return valid_actions[torch.argmax(valid_q_values)]
+        
+    # replay buffer에 경험 추가 
+    def append_memory(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    
+    # mini-batch로 업데이트 
+    def replay(self):
+        if len(self.memory) < self.batch_size*2:
+            return
+        # batch size 만큼 랜덤으로 꺼낸다 
+        minibatch = random.sample(self.memory, self.batch_size)
+
+        # state_batch.shape: (batch_size, 1, 6, 7)
+        state1_batch = torch.stack([s1 for (s1,a,r,s2,d) in minibatch]).unsqueeze(1).to(self.device)
+        # action_batch.shape: (batch_size, )
+        action_batch = torch.Tensor([a for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        reward_batch = torch.Tensor([r for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        state2_batch = torch.stack([s2 for (s1,a,r,s2,d) in minibatch]).unsqueeze(1).to(self.device)
+        done_batch = torch.Tensor([d for (s1,a,r,s2,d) in minibatch]).to(self.device)
+        # print("state1_batch:",state1_batch.shape)
+        Q1 = self.policy_net(state1_batch)  # (256,7)
+        with torch.no_grad():
+            Q2 = self.target_net(state2_batch)
+        # target Q value들 
+        Y = reward_batch + self.gamma * ((1-done_batch) * torch.max(Q2,dim=1)[0])
+        # 해당하는 action을 취한 q value들
+        X = Q1.gather(dim=1,index=action_batch.long().unsqueeze(dim=1)).squeeze()
+        
+        
+        loss = nn.MSELoss()(X, Y.detach())
+        # print("to compare overestimation of Q value")
+        # print(state1_batch[200][0])
+        # print(state2_batch[200][0])
+        # print("action:",action_batch[200])
+        # print("reward:",reward_batch[200])
+        # print(Q1[200])
+        # print(Q2[200])
+        # print()
+
+        # tensor.numpy()는 cpu에서만 가능하므로 cpu() 처리
+        self.losses.append(loss.detach().cpu().numpy())
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # state, action, reward, next_state, done = zip(*minibatch)
+        # print(state)
+        # state = torch.FloatTensor(state) # .to(self.device)
+
+        # action = torch.LongTensor(action).unsqueeze(1)  #  .to(self.device)
+        # reward = torch.FloatTensor(reward).unsqueeze(1)  # .to(self.device)
+        # next_state = torch.FloatTensor(next_state)  # .to(self.device)
+        # done = torch.FloatTensor(done).unsqueeze(1)  # .to(self.device)
+        
+        # current_q_values = self.policy_net(state).gather(1, action)
+        # next_q_values = self.target_net(next_state).detach().max(1)[0].unsqueeze(1)
+        # expected_q_values = reward + self.gamma * next_q_values * (1 - done)
+        
+        # loss = nn.MSELoss()(current_q_values, expected_q_values)
+        # self.optimizer.zero_grad()
+        # loss.backward()
+        # self.optimizer.step()
+        self.steps += 1
+
+        # 일단 episode 기준으로 target net update를 할 예정이므로 미적용 
+        # if self.steps % self.target_update == 0:
+        #     self.update_target_net()
+        
+    # target net에 policy net 파라미터 들을 업데이트 해줌 
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.policy_net.state_dict())
 
 
 
--- train.py
+++ train.py
@@ -8,20 +8,38 @@ import time
 import matplotlib.pyplot as plt
 import os
 
-epi = 10000
+epi = 50000
 # 상대를 agent의 policy로 동기화 시키는건 편향이 세지므로 일단 제외
-op_update = 100
-CFenv = env.ConnectFourEnv()
-Qagent = env.ConnectFourDQNAgent()
-Qagent2 = env.ConnectFourDQNAgent(eps=1)  # it means Qagent2 has random policy
-losses = []
+# op_update = 100
+CFenv = env.ConnectFourEnv()  # connext4 환경 생성
+Qagent = env.ConnectFourDQNAgent_CNN()  #학습시킬 agent
+# Qagent2 = env.ConnectFourDQNAgent(eps=1)  # it means Qagent2 has random policy
+Qagent2 = env.ConnectFourDQNAgent_CNN()  # 상대 agent
+losses = []  # loss 값 plot을 위한 list
+noise=False  # board를 normalize 할 때 noise 추가 여부.
+flatten = False  # cnn일 땐 False, linear일 땐 true
+target_update = 500  # target net을 update 하는 주기(단위: episode)
 
-def board_normalization(arr):
+# 2를 -1로 바꿔서 board를 -1~1로 바꿔줌
+def board_normalization(noise,env=CFenv, flatten=True):
+    # cnn을 사용하지 않는다면, 2차원 board를 1차원으로 바꿔줘야됨 
+    if flatten:
+        arr = copy.deepcopy(env.board.flatten())
+    else: arr = copy.deepcopy(env.board)
+
+
     """Replace all occurrences of 2 with -1 in a numpy array"""
-    arr2 = copy.deepcopy(arr)
-    arr2[arr2 == 2] = -1
-    return arr2
+    arr[arr == 2] = -1
+    
+    # 2p이면 보드판을 반전시켜서 보이게 하여, 항상 같은 색깔을 보면서 학습 가능
+    if env.player == 2: arr = -1 * arr
 
+    if noise:
+        arr += np.random.randn(*arr.shape)/100.0
+
+    return arr
+
+# 모델을 pth 파일로 저장
 def save_model(model, filename='DQNmodel'):
     model_path = 'model/'+filename+'.pth'
     if os.path.isfile(model_path):
@@ -30,93 +48,200 @@ def save_model(model, filename='DQNmodel'):
             new_name = input('Enter name of new model:')
             model_path = 'model/'+new_name+'.pth'
     
-
     torch.save(model.state_dict(), 'model/'+filename+'.pth')
 
+# 모델 load. 매개변수만 load 하는게 overload가 적다고 하여 이 방법을 선택하였음 
 def load_model(model, filename='DQNmodel'):
     model.load_state_dict(torch.load('model/'+filename+'.pth'))
 
+
+# 두 모델의 승률을 비교하는 함수
+# n_battle 만큼 서로의 policy로 대결하여 
+# [model1's win, model2's win, draw] 리스트를 리턴 
 def compare_model(model1, model2, n_battle=10):
-    model1.eps, model2.eps = 0, 0  # no exploration
+    # epsilon을 복원하지 않으면, 학습 내내 고정됨 
+    eps1, eps2 = model1.eps, model2.eps
+    # 현재 model2를 random policy로 둘 예정이므로, eps=1을 사용
+    model1.eps, model2.eps = 0, 1.  # no exploration
     models = [model1, model2]
     records = [0,0,0]  # model1 win, model2 win, draw
     comp_env = env.ConnectFourEnv()
+
     for round in range(n_battle):
         comp_env.reset()
+
         while not comp_env.done:
-            state_ = board_normalization(comp_env.board.flatten())
+            # 성능 평가이므로, noise를 주지 않음 
+            state_ = board_normalization(noise=False,env=comp_env, flatten=flatten)
             state = torch.from_numpy(state_).float()
-            action = models[comp_env.player-1].select_action(state, valid_actions=comp_env.valid_actions, player=comp_env.player)
+            player = comp_env.player
+            action = models[player-1].select_action(state, valid_actions=comp_env.valid_actions, player=player)
             comp_env.step(action)
         
         if comp_env.win == 1: records[0] += 1
         elif comp_env.win == 2: records[1] += 1
         else: records[2] += 1
 
+    model1.eps, model2.eps = eps1, eps2  # restore exploration
+
     return records
 
 CFenv.reset()
 
+# 1p는 Qmodels[1], 2p는 Qmodels[2] 로 직관적으로 사용하기 위해 
+Qmodels = [0, Qagent, Qagent2]
 
 for i in range(epi):
-    if i%100==0: 
-        CFenv.print_board()
-        print("epi",i)
+    # 100번마다 loss, eps 등의 정보 표시
+    if i!=0 and i%100==0: 
+        CFenv.print_board(clear_board=False)
+        print("epi:",i, ", agent's step:",Qagent.steps)
+        # 얼마나 학습이 진행되었는지 확인하기 위해, 모델 성능 측정 
+        record = compare_model(Qagent, Qagent2, n_battle=100)
+        print(record)
+        print("agent의 승률이 {}%".format(int(100*record[0]/sum(record))))
+        print("loss:",sum(Qagent.losses[-101:-1])/100.)
+        print("epsilon:",Qagent.eps)
+
+    # 주기적으로 target net  업데이트 
+    if i%target_update==0:
+        print("target net update")
+        Qagent.update_target_net()
+        Qagent2.update_target_net()
 
     CFenv.reset()
-    if CFenv.player == 2:
-        first_move = np.random.choice(range(CFenv.n_col))
-        CFenv.board[CFenv.n_row-1, first_move] = 1
-    state_ = board_normalization(CFenv.board.flatten()) + np.random.randn(1, Qagent.state_size)/100.0
-    state = torch.from_numpy(state_).float()
+    # if CFenv.player == 2:
+    #     first_move = np.random.choice(range(CFenv.n_col))
+    #     CFenv.board[CFenv.n_row-1, first_move] = 1
+    state_ = board_normalization(noise,CFenv, flatten)
+    state = torch.from_numpy(state_).float()  # np array to torch
     done = False
+    
+    past_state, past_action, past_reward, past_done = state, None, None, done
+
     while not done :
+        # print(i)
+        # for param in Qagent.parameters():
+        #     print(type(param),  param.size())
+        
         # q_value = Qagent.policy_net(state)
         # q_value_ = q_value.data.numpy()
+        player = CFenv.player
+        op_player = 2//player
 
-        action = Qagent.select_action(state, valid_actions=CFenv.valid_actions, player=CFenv.player)
+        action = Qmodels[player].select_action(state, valid_actions=CFenv.valid_actions, player=player)
+
         observation, reward, done =  CFenv.step(action)
-        op_state_ = board_normalization(observation.flatten()) + np.random.randn(1, Qagent.state_size)
+        op_state_ = board_normalization(noise,CFenv, flatten)
         op_state = torch.from_numpy(op_state_).float()
-        if done:
-            Qagent.append_memory(state,action, reward, op_state, done)
-            break
-
-        op_action = Qagent2.select_action(op_state,valid_actions=CFenv.valid_actions, player=CFenv.player)
-        op_observation, op_reward, op_done = CFenv.step(op_action)
         
-        next_state_ = board_normalization(op_observation.flatten()) + np.random.randn(1, Qagent.state_size)
-        next_state = torch.from_numpy(next_state_).float()
-        if op_done:
-            Qagent.append_memory(state,action, -op_reward, next_state, op_done)
-            break
-
-        exp = (state, action, reward, next_state, done)
-        Qagent.append_memory(*exp)
-        state = next_state
         
+        if past_action != None:  # 맨 처음이 아닐 때 
+            # 경기가 끝났을 때(중요한 경험)
+            if done:
+                repeat = 1
+                # 중요한 경험일 때는 더 많이 memory에 추가해준다(optional)
+                if reward > 0: repeat = 5
+                for j in range(repeat):
+                    # 돌을 놓자마자 끝났으므로, next_state가 반전됨, 따라서 -1을 곱해준다
+                    Qagent.append_memory(state,action, reward, op_state*-1, done)
+                    # Qmodels[player].append_memory(state,action, reward, op_state*-1, done)
+                    # 내가 이겼으므로 상대는 음의 보상을 받음 
+                    #Qmodels[op_player].append_memory(past_state, past_action, -reward, op_state, done)
+                    Qagent.append_memory(past_state, past_action, -reward, op_state, done)
 
-        Qagent.replay()
+                # print for debugging
+                # print("for player")
+                # print("state:\n",torch.round(state).int())
+                # print("action:",action)
+                # print("reward:",reward)
+                # print("next_state\n",torch.round(op_state*-1).int())
+                # print()
+                # print("for opponent")
+                # print("state:\n",torch.round(past_state).int())
+                # print("action:",past_action)
+                # print("reward:",-reward)
+                # print("next_state\n",torch.round(op_state).int())
+                # print()
 
-        if done: break
+            # 경기가 끝나지 않았다면
+            else:
+                Qagent.append_memory(past_state, past_action, past_reward, op_state, past_done)
+                # Qmodels[op_player].append_memory(past_state, past_action, past_reward, op_state, past_done)
 
-    if Qagent.eps > 0.1: Qagent.eps -= (1/epi)
+                # print for debugging
+                # print("for opponent")
+                # print("state:\n",torch.round(past_state).int())
+                # print("action:",past_action)
+                # print("reward:",past_reward)
+                # print("next_state\n",torch.round(op_state).int())
+                # print()
 
-    if i>= epi//2 and i%op_update==0: 
-        records = compare_model(Qagent, Qagent2)
+        
+        # op_action = Qmodels[player].select_action(op_state,valid_actions=CFenv.valid_actions, player=player)
+        # op_observation, op_reward, op_done = CFenv.step(op_action)
+        
+        # next_state_ = board_normalization(op_observation.flatten()) + np.random.randn(1, Qagent.state_size)/100.0
+        # next_state = torch.from_numpy(next_state_).float()
+        # # 2p가 돌을 놓자마자 끝남 
+        # if op_done:
+        #     Qmodels[player].append_memory(op_state,op_action, op_reward, next_state, op_done)
+        #     Qmodels[op_player].append_memory(state,action, -op_reward, next_state, op_done)
+        # else:
+        #     exp = (state, action, reward, next_state, done)
+        #     Qmodels[player].append_memory(*exp)
 
-        if records[0] >= records[1]:
-            Qagent2.policy_net.load_state_dict(Qagent.policy_net.state_dict())
+        # info들 업데이트 해줌 
+        past_state = state
+        past_action = action
+        past_reward = reward
+        past_done = done
+        state = op_state
+        
+        # replay buffer 를 이용하여 mini-batch 학습
+        Qmodels[player].replay()
+        Qmodels[op_player].replay()
+        # if Qagent.memory and abs(Qagent.memory[-1][2])!=1:
+        #     print("state:\n",torch.round(Qagent.memory[-1][0]).int())
+        #     print("action:",Qagent.memory[-1][1])
+        #     print("reward:",Qagent.memory[-1][2])
+        #     print("next_state\n",torch.round(Qagent.memory[-1][3]).int())
             
-        else: Qagent.policy_net.load_state_dict(Qagent2.policy_net.state_dict())
+        # 게임이 끝났다면 나가기 
+        if done: break
+    # print("eps:",Qagent.eps)
+    # epsilon-greedy
+    # min epsilon을 가지기 전까지 episode마다 조금씩 낮춰준다(1 -> 0.1)
+    if Qagent.eps > 0.1: 
+        Qagent.eps -= (1/epi)
+        # Qagent2.eps -= (1/epi)
+        
 
-        Qagent2.eps = Qagent.eps
+    # if i>= epi//2 and i%op_update==0: 
+    #     records = compare_model(Qagent, Qagent2)
+
+    #     if records[0] >= records[1]:
+    #         Qagent2.policy_net.load_state_dict(Qagent.policy_net.state_dict())
+            
+    #     else: Qagent.policy_net.load_state_dict(Qagent2.policy_net.state_dict())
+
+    #     Qagent2.eps = Qagent.eps
         
 
 
 
 plt.plot(Qagent.losses)
 plt.show()
+
+
+record = compare_model(Qagent, Qagent2, n_battle=100)
+print(record)
+if record[0] >= record[1]:
+    print("Q1의 승률이 {}, Q1을 선택하겠습니다".format(record[0]/sum(record)))
+else:
+    print("Q2의 승률이 {}, Q2를 선택하겠습니다".format(record[1]/sum(record)))
+    Qagent = Qagent2
+
 
 # for testing
 mode = input("put 1 for test:\n")
@@ -128,7 +253,12 @@ if mode == '1':
 
     while CFenv.win==0:
         if CFenv.player==1:
-            col = int(input("어디에 둘 지 고르세요[0~{}]:\n".format(CFenv.n_col-1)))
+            col = input("어디에 둘 지 고르세요[0~{}]:\n".format(CFenv.n_col-1))
+            if col.isdecimal(): col = int(col)
+            else:
+                print("잘못된 입력입니다. 다시 입력해주세요.") 
+                continue
+
             if col>=CFenv.n_col or col<0:
                 print("잘못된 숫자입니다. 다시 골라주세요")
                 continue
@@ -139,7 +269,7 @@ if mode == '1':
             CFenv.step_human(col)
         else:
             time.sleep(1)
-            state_ = board_normalization(CFenv.board.flatten())
+            state_ = board_normalization(False,CFenv, flatten)
             state = torch.from_numpy(state_).float()
             action = Qagent.select_action(state, valid_actions=CFenv.valid_actions, player=CFenv.player)
             CFenv.step(action)
