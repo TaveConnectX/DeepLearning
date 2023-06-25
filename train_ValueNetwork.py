255695bfc69182befbf4d4bca588aeaa3877b1c9
Lee Dong Jun <ldj6192@gmail.com>
2023-06-25 17:12:37 +0900

    v0.01 for ValueNetwork

--- train_ValueNetwork.py
+++ train_ValueNetwork.py
@@ -1,206 +1,245 @@
+from functions import get_model_and_config_name, board_normalization, \
+                        load_model
 import env
-import AlphaZeroenv
-import copy
-import json
-import numpy as np
+from agent_structure import ConnectFourDQNAgent
+import random
 import torch
-import random
-from collections import deque
-from torch import nn, optim
-import time
-import matplotlib.pyplot as plt
-import os
-from models import DQNModel
-from ReplayBuffer import RandomReplayBuffer
-from functions import get_model_config, save_model, \
-                        get_current_time, get_model_and_config_name, load_model, board_normalization, \
-                        set_optimizer
-from agent_structure import ConnectFourDQNAgent, HeuristicAgent, set_op_agent, evaluate_model
-import random
+import torch.nn as nn
+import torch.optim as optim
+
+'''
+# 학습 데이터
+data = [
+    ([[[0, 1, 0, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1],
+       [0, 1, 0, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1],
+       [0, 1, 0, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1]]], [1,0,0]), #win
+
+    ([[[1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 0, 1, 0, 1, 0],
+       [1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 0, 1, 0, 1, 0],
+       [1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 0, 1, 0, 1, 0]]], [0,1,0]), #draw
+
+    ([[[1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 0, 0, 0]]], [0,0,1]) #lose
+]
+'''
+
+# 신경망 모델 정의
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.fc = nn.Linear(42, 3)  # 입력 크기: 7, 출력 크기: 클래스 수
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)  # 2차원 배열을 1차원으로 평탄화
+        x = self.fc(x)
+        return x
+
+# 모델 인스턴스 생성
+model = Classifier()
+
+# 손실 함수 및 최적화 기법 정의
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+'''
+# 예측
+test_data = [
+    [[[0, 1, 0, 1, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1]]],
+
+    [[[1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0]]]
+]
+
+for inputs in test_data:
+    inputs = torch.FloatTensor(inputs)
+    outputs = model(inputs)
+    probabilities = torch.sigmoid(outputs)  # 시그모이드를 적용하여 확률을 얻습니다.
+    _, predicted = torch.max(probabilities.data, 1)
+    print("Predicted Label:", predicted.item())
+'''
+
+# Value Network를 구현하기 위한 모델들은 model/models_for_V_net에 저장되어 있음
+# SL model: model/models_for/V_net/model_SL
+# RL model: model/models_for/V_net/model_RL
+
+# 각 폴더엔 모델 파일 '.pth' 과 모델의 정보를 담은 '.json' 파일로 이루어져 있음 
+
+
+# 1. Supervised Learning Model 불러오기
+# models_for_V_net 에서 모델과 config 를 불러온다.
+folder_path = 'model/models_for_V_net'
+SL_model_name, SL_model_config = get_model_and_config_name(folder_path+'/model_SL')
+# print(SL_model_name,SL_model_config)
+# 불러온 config 파일로 모델 껍데기를 만듦
+SL_agent = ConnectFourDQNAgent(config_file_name=folder_path+'/model_SL/'+SL_model_config)
+# 불러온 모델 파일로 모델 업로드
+load_model(SL_agent.policy_net, filename=folder_path+'/model_SL/'+SL_model_name)
+
+# 2. Reinforcement Learning Model 불러오기
+RL_model_name, RL_model_config = get_model_and_config_name(folder_path+'/model_RL')
+# print(RL_model_name,RL_model_config)
+RL_agent = ConnectFourDQNAgent(config_file_name=folder_path+'/model_RL/'+RL_model_config)
+load_model(RL_agent.policy_net, filename=folder_path+'/model_RL/'+RL_model_name)
+
+SL_agent.eps, RL_agent.eps = 0.0, 0.0  # greedy 한 action을 취하기 위해서
+
+
+# 환경 생성 
+VEnv = env.ConnectFourEnv()
+
+# 쓸만한 기능들
+
+# 환경 초기화
+data = list()
+for i in range(0, 2000) :
+    if(i % 100 == 0) :
+        print("making_data : " + str(i))
+    VEnv.reset()
+
+    state_ = board_normalization(noise=False, env=VEnv, use_conv=SL_agent.use_conv)
+    state = torch.from_numpy(state_).float()
+
+    use_rl = False  # RL 에이전트를 사용할 턴인지 여부
+    rl_start_turn = random.randint(15, 20)  # RL 에이전트를 사용하기 시작하는 턴
+
+    for turn in range(1, 41):
+        #print("Turn:", turn)
+        # SL 에이전트 사용 (1 ~ rl_start_turn 수)
+        if turn < rl_start_turn :
+            action = SL_agent.select_action(state=state, env=VEnv, player=VEnv.player)
+            if SL_agent.use_minimax: 
+                op_action_prediction = action[1]  # 상대 예상 액션은 이 파일에서 필요 없을 것으로 보임(아마?)
+                action = action[0]  # action은 이제 0~6 의 정수가 됨 
+        # RL 에이전트 사용 (rl_start_turn 수 이후)
+        else:
+            if use_rl:
+                action = RL_agent.select_action(state=state, env=VEnv, player=VEnv.player)
+                if RL_agent.use_minimax: 
+                    op_action_prediction = action[1]  # 상대 예상 액션은 이 파일에서 필요 없을 것으로 보임(아마?)
+                    action = action[0]  # action은 이제 0~6 의 정수가 됨 
+            else:
+                action = random.randint(0, 6)  # 랜덤으로 액션 선택
+
+        #print("Selected Action:", action)
+
+        # 선택한 action으로 환경 진행
+        state, reward, done = VEnv.step(action)
+
+        # RL 에이전트 사용 여부 결정
+        if turn == rl_start_turn:
+            use_rl = True
+
+        if done:
+            if (reward == 0.0) :
+                reward = 0
+            
+            if (VEnv.player == 2 and reward == 1) :
+                reward = -1
+
+            state = state.tolist()
+            for i in range(len(state)) :
+                for j in range(0, len(state[i])) :
+                    if state[i][j] == 2 :
+                        state[i][j] = -1
+                    state[i][j] = int(state[i][j])
+            tmp = list()
+            tmp_forlist = list()
+            tmp_forlist.append(state)
+            tmp.append(tmp_forlist)
+            if (reward == 1) :
+                tmp.append([1,0,0])
+            elif (reward == 0) : 
+                tmp.append([0,1,0])
+            elif (reward == -1) : 
+                tmp.append([0,0,1])
+            #print(data)
+            data.append(tmp)
+            break
+
+# 학습
+for i in range(0, 1600) :
+    if(i % 100 == 0) :
+        print("training : " + str(i))
+    inputs = torch.FloatTensor(data[i][0])
+    targets = torch.FloatTensor(data[i][1]).unsqueeze(0)  # 타겟에 차원을 추가합니다.
+
+    # 경사 초기화
+    optimizer.zero_grad()
+
+    # 순전파 + 역전파 + 최적화
+    #print(inputs)
+    #print(targets)
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+    loss.backward()
+    optimizer.step()
+
+# 테스트
+count = 0
+for i in range(1600, len(data)) :
+    if(i % 100 == 0) :
+        print("test : " + str(i))
+    inputs = torch.FloatTensor(data[i][0])
+    outputs = model(inputs)
+    probabilities = torch.sigmoid(outputs)  # 시그모이드를 적용하여 확률을 얻습니다.
+    _, predicted = torch.max(probabilities.data, 1)
+    Predicted_label = predicted.item()
+    #print("Predicted Label:", Predicted_label)
+    if data[i][1] == [1,0,0] :
+        real_label = 1
+    elif data[i][1] == [0,1,0] :
+        real_label = 0
+    elif data[i][1] == [0,0,1] :
+        real_label = 2
+    #print("real Label:", real_label)
+    if (Predicted_label == real_label) :
+        count += 1
+print("percent : " + str(count/400*100))
 
     
-#Value_Network
-class ConnectFourValueAgent(nn.Module) :
-    def __init__(self, state_size=6*7, action_size=7, config_file_name=None, **kwargs):
-        super(ConnectFourValueAgent,self).__init__()
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        config = get_model_config(config_file_name)
-        for key, value in kwargs.items():
-            config[key] = value
+'''
+# 학습 데이터
+data = [
+    ([[[0, 1, 0, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1],
+       [0, 1, 0, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1],
+       [0, 1, 0, 1, 1, 0, 1],
+       [1, 0, 1, 0, 1, 0, 1]]], [1,0,0]), #win
 
-        self.use_conv=config['use_conv']
-        self.use_resnet=config['use_resnet']
-        self.use_minimax=config['use_minimax']
+    ([[[1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 0, 1, 0, 1, 0],
+       [1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 0, 1, 0, 1, 0],
+       [1, 0, 1, 1, 0, 1, 0],
+       [0, 1, 0, 1, 0, 1, 0]]], [0,1,0]), #draw
 
-        self.policy_net = DQNModel(use_conv=self.use_conv,
-                                   use_resnet=self.use_resnet,
-                                   use_minimax=self.use_minimax
-                                   ).model
-        # 실제 업데이트되는 network
-
-        # target network
-        self.target_net = copy.deepcopy(self.policy_net)
-        # deepcopy하면 파라미터 load를 안해도 되는거 아닌가? 일단 두자
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.target_net.eval()
-
-        self.lr = config['lr']
-
-        # optimizer는 기본적으로 adam을 사용하겠지만 추후 다른 것으로 실험할 수도 있음
-        self.optimizer = set_optimizer(
-            config['optimizer'], 
-            self.policy_net.parameters(), 
-            lr=self.lr
-        )
-        
-        self.selfplay = config['selfplay']
-        self.add_pool_freq = config['add_pool_freq']  # no need if no selfplay
-
-        self.memory_len = config['memory_len']
-        self.batch_size = config['batch_size']
-        # DQN에 사용될 replay memory(buffer)
-        self.memory = RandomReplayBuffer(
-            buffer_size=self.memory_len,
-            batch_size=self.batch_size,
-            use_conv=self.use_conv,
-            use_minimax=self.use_minimax
-        )
-        self.gamma = config['gamma']  # discount factor
-        self.state_size = state_size
-        self.action_size = action_size
-        self.target_update = config['target_update']  # target net update 주기(여기선 step)
-        self.steps = 0
-
-        self.epi = config['epi']
-        self.softmax_const = config['softmax_const']
-        self.max_temp = config['max_temp']
-        self.min_temp = config['min_temp']
-        self.temp_decay = (self.min_temp/self.max_temp)**(1/(self.epi*self.softmax_const))
-        self.temp = self.max_temp
-        self.eps = config['eps']  # DQN에서 사용될 epsilon
-        
-        self.batch_size = config['batch_size']  # size of mini-batch
-        self.repeat_reward = config['repeat_reward']  # repeat reward
-        self.losses = []  # loss들을 담는 list 
-        # record of win rate of random, heuristic, minimax agent
-        self.record = [[], [], []]
-
-    def train(self,epi) :
-        # 1. Supervised Learning Model 불러오기
-        # models_for_V_net 에서 모델과 config 를 불러온다.
-        folder_path = 'model/models_for_V_net'
-        SL_model_name, SL_model_config = get_model_and_config_name(folder_path+'/model_SL')
-        # print(SL_model_name,SL_model_config)
-        # 불러온 config 파일로 모델 껍데기를 만듦
-        SL_agent = ConnectFourDQNAgent(config_file_name=folder_path+'/model_SL/'+SL_model_config)
-        # 불러온 모델 파일로 모델 업로드
-        load_model(SL_agent.policy_net, filename=folder_path+'/model_SL/'+SL_model_name)
-
-        # 2. Reinforcement Learning Model 불러오기
-        RL_model_name, RL_model_config = get_model_and_config_name(folder_path+'/model_RL')
-        # print(RL_model_name,RL_model_config)
-        RL_agent = ConnectFourDQNAgent(config_file_name=folder_path+'/model_RL/'+RL_model_config)
-        load_model(RL_agent.policy_net, filename=folder_path+'/model_RL/'+RL_model_name)
-
-        SL_agent.eps, RL_agent.eps = 0.0, 0.0  # greedy 한 action을 취하기 위해서
-
-        # 환경 생성 
-        VEnv = env.ConnectFourEnv()
-
-        VEnv.reset()
-
-        # models 딕셔너리는 전역 변수로 사용하므로, players로 변경 
-        #players = {1: self, 2: op_model}
-
-        for i in range(epi):
-            state_ = board_normalization(noise=False, env=VEnv, use_conv=SL_agent.use_conv)
-            state = torch.from_numpy(state_).float()
-
-            # 100번마다 loss, eps 등의 정보 표시
-            if i!=0 and i%100==0: 
-                #env.print_board(clear_board=False)
-                print("epi:",i, ", agent's step:",self.steps)
-                # 얼마나 학습이 진행되었는지 확인하기 위해, 모델 성능 측정 
-                #evaluate_model(self, record=self.record)
-                
-                #print(self.record[0][-1],self.record[1][-1], self.record[2][-1])
-                # print("agent의 승률이 {}%".format(int(100*record[0]/sum(record))))
-                print("loss:",sum(self.losses[-101:-1])/100.)
-                if self.eps > self.softmax_const: print("epsilon:",self.eps)
-                #else: print("temp:",self.temp)
-                # simulate_model() 을 실행시키면 직접 행동 관찰 가능
-                # simulate_model(self, op_model)
-
-            
-            VEnv.reset()
-
-            state_ = board_normalization(noise=False, env=VEnv, use_conv=SL_agent.use_conv)
-            state = torch.from_numpy(state_).float()
-            use_rl = False  # RL 에이전트를 사용할 턴인지 여부
-            rl_start_turn = 19  # RL 에이전트를 사용하기 시작하는 턴
-            
-            for turn in range(1, 41):
-                if turn < 19:
-                    action = SL_agent.select_action(state=state, env=VEnv, player=VEnv.player)
-                    if SL_agent.use_minimax: 
-                        op_action_prediction = action[1]  # 상대 예상 액션은 이 파일에서 필요 없을 것으로 보임(아마?)
-                        action = action[0]  # action은 이제 0~6 의 정수가 됨 
-                    import_state = state
-                # RL 에이전트 사용 (19 수 이후)
-                else:
-                    if use_rl:
-                        action = RL_agent.select_action(state=state, env=VEnv, player=VEnv.player)
-                        if RL_agent.use_minimax: 
-                            op_action_prediction = action[1]  # 상대 예상 액션은 이 파일에서 필요 없을 것으로 보임(아마?)
-                            action = action[0]  # action은 이제 0~6 의 정수가 됨 
-                    else:
-                        action = random.randint(0, 6)  # 랜덤으로 액션 선택
-
-                #print("Selected Action:", action)
-
-                # 선택한 action으로 환경 진행
-                state, reward, done = VEnv.step(action)
-
-                #print("Observation:", state)
-                #print("Reward:", reward)
-                #print("Done:", done)
-                #print("Current Player:", VEnv.player)
-
-                # RL 에이전트 사용 여부 결정
-                if turn == rl_start_turn:
-                    import_state = state
-                    use_rl = True
-
-                if done:
-                        # 돌을 놓자마자 끝났으므로, next_state가 반전됨, 따라서 -1을 곱해준다
-                    if turn%2==1:
-                        if self.use_minimax:
-                            self.memory.add(import_state, action, op_action_prediction, reward, state*-1, done)
-                        else:
-                            self.memory.add(import_state, action, reward, state*-1, done)
-                    if turn%2==0:
-                        if self.use_minimax:
-                            self.memory.add(import_state, action, op_action_prediction, -reward, state*-1, done)
-                        else:
-                            self.memory.add(import_state, action, -reward, state*-1, done)                
-                # 게임이 끝났다면 나가기 
-                if done:
-                    break
-            # print("eps:",Qagent.eps)
-            
-            # epsilon-greedy
-            # min epsilon을 가지기 전까지 episode마다 조금씩 낮춰준다(1 -> 0.1)
-            if self.eps > 0.1: self.eps -= (1/epi)
-            if self.eps < self.softmax_const:
-                self.temp *= self.temp_decay
-
-
-if __name__ == "__main__" :
-    config = get_model_config()
-    CFenv = env.ConnectFourEnv()  # connext4 환경 생성
-    Qagent = ConnectFourValueAgent(
-        state_size=CFenv.n_col * CFenv.n_row,
-        action_size=CFenv.n_col
-    )
-    Qagent.train(epi=config['epi'])
+    ([[[1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 1],
+       [0, 0, 0, 0, 0, 0, 0]]], [0,0,1]) #lose
+]
+print(type(data[0]))
+'''
